1273f7c56756ccc9aac7a57166ff1c69dab44ce2
oliveszn <ekpealamicheal@gmail.com>
2024-05-29 08:15:34 +0100

    idk

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -8,7 +8,8 @@ const Home = () => {
 
       <p className="text-center w-1/2 mx-auto font-medium text-textColor leading-[156.7%] font-Sat text-xl">
         Flip through more than 10,000 vintage shots, old photograghs, historic
-        images and captures seamlessly in one place. Register to get top access.
+        images and captures seamlessly in one place. Register to get top
+        access..
       </p>
     </section>
   );

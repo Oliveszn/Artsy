466f6498b18cba5bf1a061583e5ff0100fa9392e
oliveszn <ekpealamicheal@gmail.com>
2024-06-04 23:28:39 +0100

    created datafiles for drop

---
+++ client/src/datafiles/drops.js
@@ -0,0 +1,54 @@
+export const drops = [
+  {
+    id: 1,
+    title: "Eyo: Eko For Show",
+    subtitle:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quaerat a quisquam quis exercitationem. Quia alias minima, at cum saepe quam adipisci et nulla nostrum.",
+    timeNum: "6hr: 40mins: 15s",
+    timeDay: "November 21 at 11am WAT",
+    creator: "Aliya Minat",
+    condition: "Time remaining",
+    status: "UPCOMING",
+    btn: "Join",
+    image: "images/Rectangle 82.png",
+  },
+  {
+    id: 2,
+    title: "Ginger Suburbs",
+    subtitle:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quaerat a quisquam quis exercitationem. Quia alias minima, at cum saepe quam adipisci et nulla nostrum.",
+    timeNum: "6hr: 40mins: 15s",
+    timeDay: "November 21 at 11am WAT",
+    creator: "Tina Benson",
+    condition: "Time remaining",
+    status: "LIVE NOW",
+    btn: "Join",
+    image: "images/Rectangle 84.png",
+  },
+  {
+    id: 3,
+    title: "Sink",
+    subtitle:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quaerat a quisquam quis exercitationem. Quia alias minima, at cum saepe quam adipisci et nulla nostrum.",
+    timeNum: "2 hours ago",
+    timeDay: "November 21 at 11am WAT",
+    creator: "Aliya Minat",
+    condition: "Auction Ended",
+    status: "ENDED",
+    btn: "View",
+    image: "images/Rectangle 87.png",
+  },
+  {
+    id: 4,
+    title: "Warped ‘99",
+    subtitle:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quaerat a quisquam quis exercitationem. Quia alias minima, at cum saepe quam adipisci et nulla nostrum.",
+    timeNum: "2 days ago",
+    timeDay: "November 21 at 11am WAT",
+    creator: "Buju Benson",
+    condition: "Auction Ended",
+    status: "ENDED",
+    btn: "View",
+    image: "images/Rectangle 2332.png",
+  },
+];

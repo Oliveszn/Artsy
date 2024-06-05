641969c685f546f9e7170ca6550088d39fe7f769
oliveszn <ekpealamicheal@gmail.com>
2024-06-05 17:04:00 +0100

    working on drops

--- client/src/components/Drop.jsx
+++ client/src/components/Drop.jsx
@@ -5,6 +5,30 @@ import rec23 from "../images/Rectangle 2332.png";
 import { drops } from "../datafiles/drops";
 
 const Drop = () => {
+  const getStatusStyle = (status) => {
+    switch (status) {
+      case "UPCOMING":
+        return { backgroundColor: "#3EA03B" };
+      case "LIVE NOW":
+        return { backgroundColor: "#3EA03B" };
+      case "ENDED":
+        return { backgroundColor: "#999EA5" };
+      default:
+        return {};
+    }
+  };
+
+  const getBtnStyle = (btn) => {
+    switch (btn) {
+      case "Join":
+        return { backgroundColor: "#4693ED" };
+      case "View":
+        return { backgroundColor: "#999EA5" };
+      default:
+        return {};
+    }
+  };
+
   return (
     <>
       <div className="mx-auto container">
@@ -25,7 +49,7 @@ const Drop = () => {
       <section className="mx-auto container mt-20">
         {drops.map((drops) => (
           <div
-            className="flex flex-col md:flex-row gap-x-7 gap-y-7"
+            className="flex flex-col md:flex-row gap-x-7 gap-y-7 mb-10"
             key={drops.id}
           >
             <div className="relative">
@@ -34,19 +58,29 @@ const Drop = () => {
                 <div className="flex items-center time__holder py-2 px-6 rounded-lg mb-10 w-2/3">
                   <div className="flex flex-col items-start">
                     <div>
-                      <h1 className="time">{drops.condition}</h1>
+                      <h1 className="text-base md:text-2xl font-normal font-Sat">
+                        {drops.condition}
+                      </h1>
                     </div>
                     <div className="flex">
-                      <h1 className="time__num">{drops.timeNum}</h1>
-                      <button className="bg-btnColorAlt join">
+                      <h1 className="text-xl font-normal md:text-2xl md:mr-20 font-Stix">
+                        {drops.timeNum}
+                      </h1>
+                      <button
+                        style={getBtnStyle(drops.btn)}
+                        className="text-priColor py-1 px-6 rounded-xl text-xl font-Sat hidden md:block"
+                      >
                         {drops.btn}
                       </button>
                     </div>
                   </div>
                 </div>
 
-                <div className="ehen flex flex-col items-end top-5 right-8 absolute md:hidden">
-                  <button className="bg-btnColorTet update">
+                <div className="flex flex-col items-end top-5 right-8 absolute md:hidden">
+                  <button
+                    className="mb-4 text-priColor py-1 px-4 rounded-md font-Sat font-medium text-xs"
+                    style={getStatusStyle(drops.status)}
+                  >
                     {drops.status}
                   </button>
                 </div>
@@ -54,13 +88,22 @@ const Drop = () => {
             </div>
 
             <div className="md:w-1/2">
-              <button className="update__sec bg-btnColorTet">
+              <button
+                style={getStatusStyle(drops.status)}
+                className="hidden md:block mb-4 text-priColor py-1 px-8 rounded-lg font-Sat text-lg font-medium"
+              >
                 {drops.status}
               </button>
-              <p className="time__day">{drops.timeDay}</p>
-              <h1 className="title">{drops.title}</h1>
-              <p className="subtitle">{drops.subtitle}</p>
-              <p className="creator">
+              <p className="mb-4 md:text-xl text-base font-normal text-secColor font-Sat">
+                {drops.timeDay}
+              </p>
+              <h1 className="mb-4 md:text-4xl text-2xl font-medium text-secColor font-Sat">
+                {drops.title}
+              </h1>
+              <p className="md:w-2/3 mb-4 font-normal text-lg md:text-xl text-tetColor font-Sat">
+                {drops.subtitle}
+              </p>
+              <p className="mb-4 text-xl md:text-2xl font-medium text-secColor font-Sat">
                 Creator:{" "}
                 <a href="#" className="text-linkColor">
                   {drops.creator}
